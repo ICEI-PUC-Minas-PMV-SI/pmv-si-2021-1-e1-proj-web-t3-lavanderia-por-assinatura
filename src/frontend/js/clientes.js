36a0da780205eb24506bfdb8bf585d9073d54091
Bernardo Campos <62954018+bernardocamps@users.noreply.github.com>
2021-06-07 18:00:53 -0300

    ajustes css clientes

---
+++ src/frontend/js/clientes.js
@@ -0,0 +1,57 @@
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+// CREATE Contato
+function insertContato(contato) {
+    let novoContato = {
+        "nome": contato.nome,
+        "email" : contato.email,
+        "telefone": contato.telefone,
+        "cidade" : contato.cidade,
+        "categoria": contato.categoria,
+    };
+
+    // Enviando para o Backend
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "https://lavebemheroku.herokuapp.com/api/clientes",
+        data: JSON.stringify(novoContato),
+        dataType: 'json'
+    });
+
+    // Recarrega a página e mostra mensagem de sucesso
+    displayMessage("Contato inserido com sucesso");
+}
+
+// UPDATE Contato
+function updateContato(contato) {
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "https://lavebemheroku.herokuapp.com/api/clientes/update",
+        data: JSON.stringify(contato),
+        dataType: 'json'
+    });
+
+    displayMessage("Contato alterado com sucesso");
+}
+
+// DELETE Contato
+function deleteContato(campoId) {    
+
+    let deletaId = { "id": campoId };
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "https://lavebemheroku.herokuapp.com/api/clientes/delete",
+        data: JSON.stringify(deletaId),
+        dataType: 'json'
+    });
+
+    displayMessage("Contato removido com sucesso");
+}
